616023e8d69084f7ecffac28695a9161a05997e2
Dominic Kirste <dominic.kirste@deutschebahn.com>
2025-11-22 11:23:07 +0100

    example: plain markdown

---
+++ app/components/ExamplePlainMd.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import markdownit from 'markdown-it';
+
+const md = markdownit();
+const markdown = 'A link to [about](/about) *forcing* a page reload.';
+const result = md.render(markdown);
+</script>
+
+<template>
+  <h2>Plain Markdown</h2>
+  <div v-html="result"></div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <ExamplePlainMd />
+</template>
