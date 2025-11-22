85052d9d9e3df09c7a2dae6b5921a7e6178bfd4c
Dominic Kirste <dominic.kirste@deutschebahn.com>
2025-11-22 11:24:11 +0100

    example: with @nuxtjs/mdc

---
+++ app/components/ExampleMdc.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { parseMarkdown } from '@nuxtjs/mdc/runtime';
+
+const markdown = 'A link to [about](/about) *without* forcing a page reload.';
+const { data: ast } = await useAsyncData('md', () => parseMarkdown(markdown));
+</script>
+
+<template>
+  <h2>MDC</h2>
+  <MDCRenderer v-if="ast?.body" :body="ast.body" :data="ast.data" />
+</template>
